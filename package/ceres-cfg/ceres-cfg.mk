fef0595e12defbbe4a3b292ca197b3a9f49a6324
Neil Alexander <neilalexander@users.noreply.github.com>
2017-10-03 14:25:29 +0100

    Initial commit

---
+++ package/ceres-cfg/ceres-cfg.mk
@@ -0,0 +1,23 @@
+################################################################################
+#
+# ceres-cfg
+#
+################################################################################
+
+CERES_CFG_VERSION = dev-buildroot
+CERES_CFG_SITE = git://github.com/ceres/ceres-cfg.git
+CERES_CFG_SITE_METHOD = git
+
+CERES_CFG_DEPENDENCIES = python3 python-flask python-requests
+
+CERES_CFG_MAKE_ENV = $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS)
+
+define CERES_CFG_BUILD_CMDS
+	$(CERES_CFG_MAKE_ENV) $(MAKE) -C $(@D)
+endef
+
+define CERES_CFG_INSTALL_TARGET_CMDS
+	$(CERES_CFG_MAKE_ENV) $(MAKE) -C $(@D) install
+endef
+
+$(eval $(generic-package))
